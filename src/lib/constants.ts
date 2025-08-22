9657f80744b6ece6ea1de1533489e2a39b6f5578
Alvaro Resplandor <alvarojrr79@gmail.com>
2025-08-22 13:19:09 -0400

    fix: add cv file

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -5,5 +5,5 @@ export const me = {
 	email: "alvarojrr79@gmail.com",
 	linkedin: "https://www.linkedin.com/in/alvaro-resplandor/",
 	github: "https://github.com/alvaro-jrr",
-	resume: "/cv.pdf",
+	resume: "/data/cv_es.pdf",
 };
