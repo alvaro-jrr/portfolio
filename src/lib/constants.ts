26c0207d4e3735c96af0f2844959560433b45a72
Alvaro Resplandor <alvarojrr79@gmail.com>
2025-08-23 15:49:43 -0400

    feat: hero section

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,4 +3,7 @@
  */
 export const me = {
 	email: "alvarojrr79@gmail.com",
+	linkedin: "https://www.linkedin.com/in/alvaro-resplandor/",
+	github: "https://github.com/alvaro-jrr",
+	resumeUrl: "/data/cv_es.pdf",
 };
