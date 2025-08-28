9855400bf914c3856ce1f4114c4ad08cbec93f94
Alvaro Resplandor <alvarojrr79@gmail.com>
2025-08-27 21:56:17 -0400

    chore: add robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,13 @@ import tailwindcss from "@tailwindcss/vite";
 
 import sitemap from "@astrojs/sitemap";
 
+import robotsTxt from "astro-robots-txt";
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://alvaro-resplandor.vercel.app",
-	vite: {
-		plugins: [tailwindcss()],
-	},
-	integrations: [sitemap()],
+    site: "https://alvaro-resplandor.vercel.app",
+    vite: {
+        plugins: [tailwindcss()],
+    },
+    integrations: [sitemap(), robotsTxt()],
 });
